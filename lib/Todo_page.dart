3ad85da5bd70ea19ec276f9eb3bcb42ef9917f00
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-05-09 01:37:42 +0700

    membuat halaman todopage

--- lib/Todo_page.dart
+++ lib/Todo_page.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:belajarbloc/bloc/todo_bloc.dart';
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
