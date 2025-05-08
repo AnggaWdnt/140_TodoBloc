7b0511ee476b13e52fcf087eca3e318683ce6526
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-05-09 01:31:37 +0700

    first commit

---
+++ lib/bloc/todo_bloc.dart
@@ -0,0 +1,60 @@
+import 'package:belajarbloc/model/todo.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+part 'todo_event.dart';
+part 'todo_state.dart';
+
+class TodoBloc extends Bloc<TodoEvent, TodoState> {
+  TodoBloc() : super(TodoInitial()) {
+    on<TodoEventAdd>((event, emit) {
+      final currenState = state;
+      if (currenState is TodoLoaded) {
+        final List<Todo> updatedTodos = List.from(currenState.todos);
+        updatedTodos.add(
+          Todo(
+          title: event.title,
+          isCompleted: false,
+          date: event.date),
+        );
+        emit(
+          TodoLoaded(
+           todos: updatedTodos,
+           selectedDate: currenState.selectedDate,
+        ),
+        );
+      }
+    });
+
+    on<TodoSelectedDate>((event, emit){
+      final currenState = state;
+      if (currenState is TodoLoaded) {
+        emit(TodoLoaded(todos: currenState.todos, 
+        selectedDate: event.date  
+        ));
+      }
+    });
+
+    on<TodoEventComplete>((event, emit) {
+      final currenState = state;
+      if (currenState is TodoLoaded){
+        final List<Todo> updatedTodos = List.from(currenState.todos);
+        if (event.index >= 0 && event.index < updatedTodos.length){
+        updatedTodos[event.index] = Todo(
+          title: updatedTodos[event.index].title,
+          isCompleted: updatedTodos[event.index].isCompleted == true,
+        // isCompleted; !updatedTodos[event.index].isCompleted,
+        date: updatedTodos[event.index].date,
+        );
+      emit(
+        TodoLoaded(
+          todos: updatedTodos,
+          selectedDate: currenState.selectedDate,
+        ),
+      );
+    }
+  }
+});
+  }
+}
