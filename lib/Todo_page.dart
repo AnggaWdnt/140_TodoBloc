194f3f5a03f9d039651bc6dd036ee7613f578008
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-05-09 01:39:01 +0700

    form dan input

--- lib/Todo_page.dart
+++ lib/Todo_page.dart
@@ -4,3 +4,16 @@ import 'package:belajarbloc/bloc/todo_bloc.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Todo List'),
